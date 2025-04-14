1afeddcb5bef6b0d4d08f97c129d72618cf65b4b
I561746 <I561746@fontysict.nl>
2025-04-14 10:37:38 +0200

    added logic to Device class

--- NotificationApp/NotificationApp/Models/Device.cs
+++ NotificationApp/NotificationApp/Models/Device.cs
@@ -2,5 +2,13 @@
 {
     public class Device
     {
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+        public Device(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
     }
 }
