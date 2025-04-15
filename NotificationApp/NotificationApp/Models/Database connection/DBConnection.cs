042c5fa66a70181074a80dfa6f9571c72faf4463
I561746 <I561746@fontysict.nl>
2025-04-15 11:41:57 +0200

    added IRepostory interface and folder structure to model

--- NotificationApp/NotificationApp/Models/Database connection/DBConnection.cs
+++ NotificationApp/NotificationApp/Models/Database connection/DBConnection.cs
@@ -6,7 +6,7 @@ namespace NotificationApp.Models
     public class DBConnection
     {
         private static readonly string _connectionString = @"Server=mssqlstud.fhict.local;Database=dbi561741_simacdb;
-                                                            User Id=dbi561741;Password=simacpassword;Trusted_Connection=True;";
+                                                            User Id=dbi561741_simacdb;Password=simacpassword;TrustServerCertificate = true;";
 
         public static SqlConnection GetConnection()
         {
--- NotificationApp/NotificationApp/Program.cs
+++ NotificationApp/NotificationApp/Program.cs
@@ -1,3 +1,5 @@
+using NotificationApp.Models.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -25,3 +27,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
