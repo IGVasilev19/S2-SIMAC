7f66715934d027081439ea42b956f4a6b4f1f9db
Bandev,Nikola N.E. <n.bandev@student.fontys.nl>
2025-04-15 13:36:42 +0200

    Added repositories

---
+++ NotificationApp/NotificationApp/Models/Repositories/DeviceRepository.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace NotificationApp.Models.Repositories
+{
+    public class DeviceRepository : IRepository<Device>
+    {
+        public List<Device> GetAll()
+        {
+            List<Device> devices = new List<Device>();
+
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT DeviceID, Name, Location FROM Devices";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Device device = new Device(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetString(2)
+                        );
+
+                        devices.Add(device);
+                    }
+                }
+            }
+            return devices;
+        }
+
+        public Device GetById(int id)
+        {
+            using(SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT DeviceID, Name, Location FROM Devices WHERE DeviceID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Device device = new Device(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetString(2)
+                        );
+
+                        return device;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void Add(Device entity)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "INSERT INTO Devices (Name, Location) VALUES (@name, @location)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@name", entity.Name);
+                cmd.Parameters.AddWithValue("@location", entity.Location);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Device entity)
+        {
+            using(SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "UPDATE Devices SET Name = @name, Location = @location WHERE DeviceID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@name", entity.Name);
+                cmd.Parameters.AddWithValue("@location", entity.Location);
+                cmd.Parameters.AddWithValue("@id", entity.DeviceID);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "DELETE FROM Devices WHERE DeviceID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ NotificationApp/NotificationApp/Models/Repositories/NotificationRepository.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace NotificationApp.Models.Repositories
+{
+    public class NotificationRepository : IRepository<Notification>
+    {
+        public List<Notification> GetAll()
+        {
+            List<Notification> notifications = new List<Notification>();
+
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT NotificationID, Title, Content, Important, Read, Date FROM Notifications";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Notification notification = new Notification(
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetBoolean(3),
+                            reader.GetBoolean(4),
+                            reader.GetDateTime(5)
+                        );
+
+                        notifications.Add(notification);
+                    }
+                }
+            }
+            return notifications;
+        }
+
+        public Notification GetById(int id)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "SELECT NotificationID, Title, Content, Important, Read, Date FROM Notifications WHERE NotificationID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        Notification notification = new Notification(
+                            reader.GetString(1),
+                            reader.GetString(2),
+                            reader.GetBoolean(3),
+                            reader.GetBoolean(4),
+                            reader.GetDateTime(5)
+                        );
+
+                        return notification;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void Add(Notification entity)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "INSERT INTO Notifications (Title, Content, Important, Read, Date) VALUES (@title, @content, @important, @read, @date)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@title", entity.Title);
+                cmd.Parameters.AddWithValue("@content", entity.Content);
+                cmd.Parameters.AddWithValue("@important", entity.Important);
+                cmd.Parameters.AddWithValue("@read", entity.Read);
+                cmd.Parameters.AddWithValue("@date", entity.Date);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Update(Notification entity)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "UPDATE Notifications SET Title = @title, Content = @content, Important = @important, Read = @read, Date = @date WHERE NotificationID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@title", entity.Title);
+                cmd.Parameters.AddWithValue("@content", entity.Content);
+                cmd.Parameters.AddWithValue("@important", entity.Important);
+                cmd.Parameters.AddWithValue("@read", entity.Read);
+                cmd.Parameters.AddWithValue("@date", entity.Date);
+                cmd.Parameters.AddWithValue("@id", entity.NotificationID);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = DBConnection.GetConnection())
+            {
+                string query = "DELETE FROM Notifications WHERE NotificationID = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
