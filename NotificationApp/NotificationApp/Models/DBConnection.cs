dbe4cdd536b7e326f4a203a2d401851e6e551b76
I561746 <I561746@fontysict.nl>
2025-04-15 10:22:54 +0200

    added DBConnection class

---
+++ NotificationApp/NotificationApp/Models/DBConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace NotificationApp.Models
+
+{
+    public class DBConnection
+    {
+        private static readonly string _connectionString = @"Server=mssqlstud.fhict.local;Database=dbi561741_simacdb;
+                                                            User Id=dbi561741;Password=simacpassword;Trusted_Connection=True;";
+
+        public static SqlConnection GetConnection()
+        {
+            var connection = new SqlConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
