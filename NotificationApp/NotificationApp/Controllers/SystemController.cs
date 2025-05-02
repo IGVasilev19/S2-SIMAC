485c1a07dfea2c8fafe135c89ee7567e5d1acce1
I549842 <ivelin.vasilev@student.fontys.nl>
2025-05-02 12:54:43 +0200

    Refactor Views and controllers

---
+++ NotificationApp/NotificationApp/Controllers/SystemController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationApp.Controllers
+{
+    public class SystemController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+
+        public IActionResult DevicesPanel()
+        {
+            return View();
+        }
+
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
+        public IActionResult AdminCreateEditPanel()
+        {
+            return View();
+        }
+    }
+}
