cb4c11dcd23a47c920191f361a9fa129faf796c8
i561072 <i561072@fontysict.nl>
2025-05-13 11:29:56 +0200

    Fix frontend

--- NotificationApp/BLL/Role.cs
+++ NotificationApp/BLL/Role.cs
@@ -7,6 +7,8 @@ namespace BLL
         public int RoleId { get; set; }
         public string Name { get; set; }
 
+        public Role() { }
+
         public Role(int id, string name) // Taking from DB
         {
             RoleId = id;
