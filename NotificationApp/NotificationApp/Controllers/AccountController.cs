384b58a9e01e7b6d133027b124732e5b75387c56
I549842 <ivelin.vasilev@student.fontys.nl>
2025-05-21 10:14:04 +0200

    Separate Account pages from System controller

---
+++ NotificationApp/NotificationApp/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+using Azure.Identity;
+using BLL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using NotificationApp.Models;
+using NotificationApp.Models.DTO_View_Models;
+using Service.Interfaces;
+using System.Collections.Specialized;
+using System.Security.Claims;
+
+namespace NotificationApp.Controllers
+{
+    public class AccountController : Controller
+    {
+
+    }
+}
