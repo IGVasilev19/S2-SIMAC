d55cc774f931edd668eabba0547d93b2e9b82c12
i561072 <i561072@fontysict.nl>
2025-05-23 11:52:15 +0200

    removed unnecassary lines of code, and made Admin panel

---
+++ NotificationApp/NotificationApp/Models/AdminPanelViewModel.cs
@@ -0,0 +1,9 @@
+﻿using NotificationApp.Models.DTO_View_Models;
+
+namespace NotificationApp.Models
+{
+    public class AdminPanelViewModel
+    {
+        public List<OrganizationViewModel> Organizations {  get; set; }
+    }
+}
