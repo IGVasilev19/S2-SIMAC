e1ef8ea9ff88124adfeebdaf966fd314e27a2d6d
i561072 <i561072@fontysict.nl>
2025-05-23 12:46:38 +0200

    Changes to Admin panel

---
+++ NotificationApp/NotificationApp/Models/AdminCreatePanelViewModel.cs
@@ -0,0 +1,13 @@
+using NotificationApp.Models;
+
+namespace NotificationApp.Models
+{
+    public class AdminCreatePanelViewModel
+    {
+        public string OrganizationName {  get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
