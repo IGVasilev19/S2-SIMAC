9abf915ac7625069d5d61846c557caf92fd5b9ea
I549842 <ivelin.vasilev@student.fontys.nl>
2025-05-23 13:33:49 +0200

    Fix Inbox and dropdown for account pages

--- NotificationApp/NotificationApp/wwwroot/js/site.js
+++ NotificationApp/NotificationApp/wwwroot/js/site.js
@@ -3,32 +3,37 @@
   const date = el.dataset.date;
   const body = el.dataset.body;
 
-    document.getElementById('preview-title').innerText = title;
-    document.getElementById('preview-date').innerText = date;
-    document.getElementById('preview-body').innerText = body;
-}
-
-document.querySelectorAll('.parent-checkbox').forEach(parent => {
-    parent.addEventListener('change', function () {
-        const containerId = this.dataset.childContainerId;
-        const childContainer = document.getElementById(containerId);
-
-        if (this.checked) {
-            childContainer.style.display = 'block';
-        } else {
-            childContainer.style.display = 'none';
-
-            // Uncheck all children
-            childContainer.querySelectorAll('.child-checkbox').forEach(child => {
-                child.checked = false;
-            });
-        }
-    });
-});
-
   document.getElementById("preview-title").innerText = title;
   document.getElementById("preview-date").innerText = date;
   document.getElementById("preview-body").innerText = body;
+
+  document.querySelectorAll(".notification").forEach((div) => {
+    div.classList.remove("bg-gray-200", "rounded");
+  });
+
+  el.querySelector(".notification").classList.add("bg-gray-200", "rounded");
+}
+
+document.querySelectorAll(".parent-checkbox").forEach((parent) => {
+  parent.addEventListener("change", function () {
+    const containerId = this.dataset.childContainerId;
+    const childContainer = document.getElementById(containerId);
+
+    if (this.checked) {
+      childContainer.style.display = "block";
+    } else {
+      childContainer.style.display = "none";
+
+      childContainer.querySelectorAll(".child-checkbox").forEach((child) => {
+        child.checked = false;
+      });
+    }
+  });
+});
+
+document.getElementById("preview-title").innerText = title;
+document.getElementById("preview-date").innerText = date;
+document.getElementById("preview-body").innerText = body;
 
 function toggleRoleDropdown() {
   const dropdown = document.getElementById("dropdownRoleContent");
