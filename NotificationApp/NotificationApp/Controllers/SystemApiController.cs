b21217a19c528b08af2429fcfeb1420d3ebb013d
I561746 <I561746@fontysict.nl>
2025-06-03 10:53:02 +0200

    Added Rest API which allows posting of notifications through api/systemapi/notification

---
+++ NotificationApp/NotificationApp/Controllers/SystemApiController.cs
@@ -0,0 +1,35 @@
+﻿using BLL;
+using Microsoft.AspNetCore.Mvc;
+using Service.Interfaces;
+
+namespace NotificationApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SystemApiController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+
+        public SystemApiController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok("API is working!");
+        }
+
+        [HttpPost("notification")]
+        public IActionResult PostNotification([FromBody] Notification notification)
+        {
+            if (notification == null)
+            {
+                return BadRequest();
+            }
+            _notificationService.AddNotification(notification);
+            return Ok("Notification added successfully!");
+        }
+    }
+}
