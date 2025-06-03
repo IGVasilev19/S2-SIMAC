1b798a2bedbefee4543ed5d7ccef76523736caa7
I549842 <ivelin.vasilev@student.fontys.nl>
2025-06-03 10:49:39 +0200

    Install Chart js in project + Add logic in Analytics controller

--- NotificationApp/NotificationApp/Controllers/AnalyticsController.cs
+++ NotificationApp/NotificationApp/Controllers/AnalyticsController.cs
@@ -8,52 +8,64 @@ namespace NotificationApp.Controllers
 {
     public class AnalyticsController : Controller
     {
-        private readonly IAnalyticsService _analyticsService;
+        private readonly INotificationService _notificationService;
 
-        public AnalyticsController(IAnalyticsService analyticsService) 
+        public AnalyticsController(INotificationService notificationService) 
         {
-            _analyticsService = analyticsService;
+            _notificationService = notificationService;
         }
+
+        public IActionResult GetEventFrequency()
+        {
+            var events = _eventService.GetAll();
+
+            var grouped = events
+                .GroupBy(e => e.Date.Date)
+                .Select(g => new
+                {
+                    Date = g.Key.ToString("yyyy-MM-dd"),
+                    Crash = g.Count(x => x.Title == "DeviceCrash"),
+                    Maint = g.Count(x => x.Title == "Maintenance"),
+                    Tamper = g.Count(x => x.Title == "Tampering")
+                }).ToList();
+
+            var response = new
+            {
+                labels = grouped.Select(g => g.Date),
+                datasets = new[]
+                {
+                    new { label = "Device Crash", data = grouped.Select(g => g.Crash) },
+                    new { label = "Maintenance", data = grouped.Select(g => g.Maint) },
+                    new { label = "Tampering", data = grouped.Select(g => g.Tamper) }
+                }
+            };
+
+            return Json(response);
+        }
+
+        public IActionResult GetEventsPerDevice()
+        {
+            var events = _eventService.GetAll();
+
+            var grouped = events
+                .GroupBy(e => e.DeviceName)
+                .Select(g => new { Device = g.Key, Count = g.Count() });
+
+            var response = new
+            {
+                labels = grouped.Select(g => g.Device),
+                datasets = new[]
+                {
+                    new { label = "Event Count", data = grouped.Select(g => g.Count) }
+                }
+            };
+
+            return Json(response);
+        }
+
         public IActionResult Analytics()
         {
-            //var devicePanels = new List<DeviceViewModel>();
-            //var notificationPanels = new List<(NotificationViewModel, int)>();
-
-            //foreach (Device device in _analyticsService.GetAll())
-            //{
-            //    // Add device to view model list
-            //    devicePanels.Add(new DeviceViewModel
-            //    {
-            //        DeviceID = device.DeviceID,
-            //        Name = device.Name,
-            //        Location = device.Location,
-            //        OrganizationID = device.OrganizationID,
-            //        DeviceStatus = device.DeviceStatus
-            //    });
-
-            //    // Add each notification as a tuple with DeviceID
-            //    foreach (Notification notification in _analyticsService.GetByDeviceID(device.DeviceID))
-            //    {
-            //        NotificationViewModel NotificationViewModel = new NotificationViewModel
-            //        {
-            //            NotificationID = notification.NotificationID,
-            //            Title = notification.Title,
-            //            Content = notification.Content,
-            //            Important = notification.Important,
-            //            Date = notification.Date.ToString("yyyy-MM-dd HH:mm")
-            //        };
-
-            //        notificationPanels.Add((NotificationViewModel, device.DeviceID));
-            //    }
-            //}
-
-            //var viewModel = new AnalyticsViewModel
-            //{
-            //    DevicePanels = devicePanels,
-            //    NotificationPanels = notificationPanels
-            //};
-
-            return View(viewModel);
+            return View();
         }
 
     }
