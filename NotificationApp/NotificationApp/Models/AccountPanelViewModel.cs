bfbe0f7b78c3b004373def72bde4c27cf8fb818c
i561072 <i561072@fontysict.nl>
2025-06-17 11:50:46 +0200

    dont know what this is

--- NotificationApp/NotificationApp/Models/AccountPanelViewModel.cs
+++ NotificationApp/NotificationApp/Models/AccountPanelViewModel.cs
@@ -3,6 +3,6 @@ namespace NotificationApp.Models
     public class AccountPanelViewModel
     {
         public List<AccountViewModel> Accounts {get; set;}
-        public string Search { get; set;}
+        public string? Search { get; set;}
     }
 }
